2523a9ec2aad71af127b7a58934bb33a3f3eb17f
Jason Wang <wjzchina2008@126.com>
2022-09-12 01:18:36 +0800

    remove zh lang

--- uprpc-web/src/pages/index.tsx
+++ uprpc-web/src/pages/index.tsx
@@ -1,5 +1,4 @@
 import {ConfigProvider} from "antd";
-import zhCN from 'antd/lib/locale/zh_CN';
 import FileTree from "@/pages/components/File";
 import {Allotment} from "allotment";
 import "allotment/dist/style.css";
@@ -11,7 +10,7 @@ import {pathsStore, tabStore, protoStore, context} from '@/stores/context'
 export default function HomePage() {
     return (
         <context.Provider value={{pathsStore, tabStore, protoStore}}>
-            <ConfigProvider locale={zhCN}>
+            <ConfigProvider>
                 <Allotment defaultSizes={[75, 220]}>
                     <Allotment.Pane minSize={320} maxSize={600}>
                         <FileTree/>
