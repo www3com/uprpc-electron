ff6ae1e6704d48a363a93f1f8a1e866e08853cd1
geekgarden <deific@126.com>
2022-09-23 23:53:19 +0800

    add release config

--- uprpc-app/src/main.ts
+++ uprpc-app/src/main.ts
@@ -4,28 +4,28 @@ const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
 function createWindow() {
-  const mainWindow = new BrowserWindow({
-    width: 1280,
-    height: 1000,
-    title: "upRpc",
-    webPreferences: {
-      preload: path.join(__dirname, "bridge.js"),
-    },
-  });
+    const mainWindow = new BrowserWindow({
+        width: 1280,
+        height: 1000,
+        title: "upRpc",
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+        },
+    });
 
-  // mainWindow.loadFile('./dist/index.html')
-  mainWindow.loadURL("http://localhost:8000");
-  // mainWindow.webContents.openDevTools();
-  router.register(mainWindow);
+    mainWindow.loadFile("./dist/index.html");
+    // mainWindow.loadURL("http://localhost:8000");
+    // mainWindow.webContents.openDevTools();
+    router.register(mainWindow);
 }
 
 app.whenReady().then(() => {
-  createWindow();
-  app.on("activate", function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
+    createWindow();
+    app.on("activate", function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
 });
 
 app.on("window-all-closed", function () {
-  app.quit();
+    app.quit();
 });
