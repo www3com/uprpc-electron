06eefd11e23a11fbdf8a87fdd836b24671adaa7b
geekgarden <deific@126.com>
2022-09-24 17:40:41 +0800

    rename main.yml to package.yml fix package

--- uprpc-web/.umirc.ts
+++ uprpc-web/.umirc.ts
@@ -1,11 +1,10 @@
 export default {
-    npmClient: 'yarn',
-    plugins: ['@umijs/plugins/dist/antd'],
+    npmClient: "yarn",
+    plugins: ["@umijs/plugins/dist/antd"],
     antd: {},
-    history: { type: 'hash' },
-    publicPath: '/',
-    routes: [
-        {path: '/', component: 'index'}
-    ],
-    theme: { '@primary-color': '#fa541c' }
+    history: { type: "hash" },
+    publicPath: "/",
+    routes: [{ path: "/", component: "index" }],
+    theme: { "@primary-color": "#fa541c" },
+    outputPath: "../uprpc-app/static",
 };
